8d4b0413b236c28b7e1b45e707c5fdd3ab400b6f
pkhiani <pavan.khiani@gmail.com>
2024-11-23 14:58:20 -0700

    swap integration

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Swap from './Swap'
 
 export function About() {
   return (
@@ -13,11 +14,10 @@ export function About() {
           </p>
         </div>
         <div className='align-center justify-center flex'>
-        <a href="https://fun.virtuals.io/agents/0x52FfF1bFDF071A43F79A3Eb45739107Fd000DbA8">
-            <button className=" bg-yellow-500 text-white px-4 py-2 rounded-full hover:bg-yellow-600 transition" >
-              Connect Wallet
-            </button>
-            </a>
+        <SwapDefault
+            from={[eth]}
+            to={[capy]}
+          /> 
         </div>
 
       </div>
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import '@coinbase/onchainkit/styles.css';
+import { Providers } from './providers/providers.tsx'; 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
